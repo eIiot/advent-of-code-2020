36c60ee37198165dedaefc26faba383187050d9a
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2021-12-28 19:20:16 -0800

    day8

---
+++ js/2021/day8/part1.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require('fs');
+var input = fs.readFileSync('./input.txt', 'utf-8');
+input = input.replace(/\r/g, "");
+var notes = input.split(/\n/).map(function (n) { return n.split(' | ').map(function (n) { return n.split(' '); }); });
+notes.forEach(function (note, index, array) {
+    array[index].shift();
+});
+notes = notes.flat(Infinity);
+console.log(notes);
+var numOfOneFourSevenEight = 0;
+notes.forEach(function (element) {
+    if (element.length == 2 || element.length == 4 || element.length == 3 || element.length == 7) {
+        numOfOneFourSevenEight++;
+    }
+});
+console.log(numOfOneFourSevenEight);
+debugger;
+//# sourceMappingURL=part1.js.map
