69576ed0f768d5f18847afeb3fa7a81650ad650a
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-08-26 09:46:26 -0700

    day 17 p1

---
+++ 2021/day17/input.ts
@@ -0,0 +1,3 @@
+export const input = "target area: x=153..199, y=-114..-75";
+
+export const examples = ["target area: x=20..30, y=-10..-5"];
---
+++ 2021/day17/part1.ts
@@ -0,0 +1,169 @@
+import { input, examples } from "./input";
+
+const parseInput = (input: string): [[number, number], [number, number]] => {
+  let input2 = "target area: x=153..199, y=-114..-75";
+  let trimmed = input2.substring(13);
+  let xY = trimmed.split(", ");
+  return xY.map((val) =>
+    val
+      .substring(2)
+      .split("..")
+      .map((num) => +num)
+  ) as [[number, number], [number, number]];
+};
+
+let targetArea = parseInput(input);
+
+const renderTrajectory = (
+  hist: [number, number][],
+  targetArea: [[number, number], [number, number]]
+) => {
+  /**
+   * [
+   *  [minX, maxX],
+   *  [minY, maxY]
+   * ]
+   */
+  let arrayCoords = JSON.parse(JSON.stringify(targetArea));
+
+  // assign the smallest
+
+  hist.forEach(([x, y]) => {
+    arrayCoords[0][0] = x < arrayCoords[0][0] ? x : arrayCoords[0][0];
+    arrayCoords[0][1] = x > arrayCoords[0][1] ? x : arrayCoords[0][1];
+    arrayCoords[1][0] = y < arrayCoords[1][0] ? y : arrayCoords[1][0];
+    arrayCoords[1][1] = y > arrayCoords[1][1] ? y : arrayCoords[1][1];
+  });
+
+  // offset all values by the - of the min values, to 0
+
+  let xOffset = -arrayCoords[0][0];
+  let yOffset = -arrayCoords[1][0];
+
+  let fixedArrayCoords = [];
+
+  fixedArrayCoords[0] = arrayCoords[0].map((x) => x + xOffset);
+  fixedArrayCoords[1] = arrayCoords[1].map((y) => y + yOffset);
+
+  hist = hist.map(([x, y]) => [x + xOffset, y + yOffset]);
+
+  let fixedTargetArea = [];
+
+  fixedTargetArea[0] = targetArea[0].map((x) => x + xOffset);
+  fixedTargetArea[1] = targetArea[1].map((y) => y + yOffset);
+
+  // create array
+
+  const renderArray = [...Array(fixedArrayCoords[1][1] + 1)].map((_) =>
+    Array(fixedArrayCoords[0][1] + 1).fill(".")
+  );
+
+  // render target area
+
+  for (let i = fixedTargetArea[1][0]; i <= fixedTargetArea[1][1]; i++) {
+    let fixedYVal = fixedArrayCoords[1][1] - i;
+
+    for (let j = targetArea[0][0]; j <= targetArea[0][1]; j++) {
+      renderArray[fixedYVal][j] = "T";
+    }
+  }
+
+  // render coords
+
+  hist.forEach(([x, y]) => {
+    renderArray[fixedArrayCoords[1][1] - y][x] = "#";
+  });
+
+  // render start point
+
+  renderArray[fixedArrayCoords[1][1] - hist[0][1]][hist[0][0]] = "S";
+
+  console.log(renderArray.map((n) => n.join("")).join("\n"));
+};
+
+const launchProbe = (initialVelocity: [number, number]) => {
+  /**
+   * [x, y]
+   */
+  let pos: [number, number] = [0, 0];
+
+  /**
+   * [x, y]
+   */
+  let velocity = JSON.parse(JSON.stringify(initialVelocity));
+
+  let i = 0;
+
+  let maxY = 0;
+
+  let hist: [number, number][] = [];
+
+  hist.push(JSON.parse(JSON.stringify(pos)));
+
+  let hitTarget = false;
+
+  while (true) {
+    i++;
+    // one step
+
+    pos[0] += velocity[0];
+
+    pos[1] += velocity[1];
+
+    if (velocity[0] > 0) {
+      velocity[0]--;
+    } else if (velocity[0] < 0) {
+      velocity[0]++;
+    }
+
+    velocity[1]--;
+
+    maxY = pos[1] > maxY ? pos[1] : maxY;
+
+    // fill hist array with the min and max values
+
+    hist.push(JSON.parse(JSON.stringify(pos)));
+
+    // check if inside target, or missed
+
+    if (
+      pos[0] >= targetArea[0][0] &&
+      pos[0] <= targetArea[0][1] &&
+      pos[1] >= targetArea[1][0] &&
+      pos[1] <= targetArea[1][1]
+    ) {
+      console.log("Inside target area at", pos);
+      console.log("after", i, "steps");
+      console.log("Initial velocity:", initialVelocity);
+      console.log("max Y:", maxY);
+
+      // console.log({ targetArea });
+      // console.log({ hist });
+
+      // renderTrajectory(hist, targetArea);
+
+      hitTarget = true;
+
+      break;
+    } else if (pos[1] < targetArea[1][0]) {
+      console.log("missed");
+      console.log("after", i, "steps");
+      console.log("Initial velocity:", initialVelocity, "\n");
+
+      // pattern
+
+      // renderTrajectory(hist, targetArea);
+
+      break;
+    }
+  }
+
+  return hitTarget;
+};
+
+let i = 1;
+let exit = false;
+while (!exit) {
+  exit = launchProbe([i, Math.abs(targetArea[1][0]) - 1]);
+  i++;
+}
