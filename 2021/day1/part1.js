e3b7aa70f723c55d5419ac34ece9976eaf6a7388
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-09-02 10:29:46 -0700

    thanks eslint

--- 2021/day1/part1.js
+++ 2021/day1/part1.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+const fs = require("fs");
 
-var input = fs.readFileSync('./input.txt','utf-8')
+var input = fs.readFileSync("./input.txt", "utf-8");
 
 // split input into arrays
 
-const measurements = input.split('\n')
+const measurements = input.split("\n");
 
 console.log(measurements);
 
@@ -12,13 +12,13 @@ var increases = 0;
 
 for (let i = 0; i < measurements.length; i++) {
   const number = measurements[i];
-  const lastNumber = measurements[i-1];
-  
+  const lastNumber = measurements[i - 1];
+
   if (parseInt(number) > parseInt(lastNumber)) {
     increases++;
   }
 }
 
-console.log(increases)
+console.log(increases);
 
 debugger;
